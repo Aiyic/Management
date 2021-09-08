d2ee3120fd295a03c6b6947b9b2208916dd6075e
Aiyic <1936014596@qq.com>
2021-09-08 21:33:34 +0800

    debug_1

--- Management/Controllers/HomeController.cs
+++ Management/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
@@ -91,7 +92,45 @@ namespace Management.Controllers
                 return RedirectToAction("Info", new { info = "Register Failed"});
             }
         }
-        
+
+
+
+        // GET: Home/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (Session["CurrentUserId"] != null && Session["CurrentUserIsAdminister"] != null)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Person person = db.Persons.Find(id);
+                if (person == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(person);
+            }
+            else
+                return RedirectToAction("Info", "Home", new { Info = "Please Login Before Operation!!!" });
+        }
+
+        /********************************************** 修改IsAdminister **************************************/
+        /**************************************** 更新Session[IsAdminister]的状态 **************************************/
+        // POST: Home/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PersonId,Password,Name,Phone,Department")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(person).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+
 
     }
 }
