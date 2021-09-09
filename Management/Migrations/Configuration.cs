511f2a07d1c8b8a668aa594c558f839cacafddab
Aiyic <62811821+Aiyic@users.noreply.github.com>
2021-09-09 19:50:37 +0800

    add seed data

--- Management/Migrations/Configuration.cs
+++ Management/Migrations/Configuration.cs
@@ -1,4 +1,4 @@
-﻿namespace Management.Migrations
+namespace Management.Migrations
 {
     using Management.Models;
     using Management.Models.Person;
@@ -16,30 +16,23 @@
         {
             AutomaticMigrationsEnabled = false;
         }
-
-        //protected override void Seed(Management.Models.ManagementDBContext context)
-        //{
-        //  This method will be called after migrating to the latest version.
-
-        //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-        //  to avoid creating duplicate seed data. E.g.
-        //
-        //    context.People.AddOrUpdate(
-        //      p => p.FullName,
-        //      new Person { FullName = "Andrew Peters" },
-        //      new Person { FullName = "Brice Lambson" },
-        //      new Person { FullName = "Rowan Miller" }
-        //    );
-        //}
         
         protected override void Seed(Management.Models.ManagementDBContext context)
         {
             var persons = new List<Person>
             {
-                new Person { PersonId = 10000, Password = "admin", Name = "admin",
+                new Person { PersonId = 1, Password = "admin", Name = "admin",
                     Phone = 110, Department = "Xidian", IsAdminister = true},
-                new Person { PersonId = 10001, Password = "10001", Name = "first_user",
-                    Phone = 120, Department = "g336", IsAdminister = false}
+                new Person { PersonId = 2, Password = "llz", Name = "llz",
+                    Phone = 260, Department = "g340", IsAdminister = false},
+                new Person { PersonId = 3, Password = "hhb", Name = "hhb",
+                    Phone = 252, Department = "g340", IsAdminister = false},
+                new Person { PersonId = 4, Password = "lmz", Name = "lmz",
+                    Phone = 238, Department = "g340", IsAdminister = false},
+                new Person { PersonId = 5, Password = "zwz", Name = "zwz",
+                    Phone = 248, Department = "g340", IsAdminister = false},
+                new Person { PersonId = 6, Password = "lk", Name = "lk",
+                    Phone = 279, Department = "g340", IsAdminister = false}
             };
 
             persons.ForEach(s => context.Persons.AddOrUpdate(p => p.PersonId, s));
@@ -48,7 +41,10 @@
             var goods = new List<Goods>
             {
                 new Goods {GoodsId = 10000, GoodsName = "Pen", GoodsNum = 10, GoodsPrice = 5, GoodsBuyTime =  DateTime.Parse("2021-09-08")},
-                new Goods {GoodsId = 10001, GoodsName = "Paper", GoodsNum = 10000, GoodsPrice = 0.01, GoodsBuyTime =  DateTime.Parse("2021-09-08")}
+                new Goods {GoodsId = 10001, GoodsName = "A4 Paper", GoodsNum = 10000, GoodsPrice = 0.01, GoodsBuyTime =  DateTime.Parse("2021-09-08")},
+                new Goods {GoodsId = 10001, GoodsName = "Disktop Folder", GoodsNum = 100, GoodsPrice = 9.9, GoodsBuyTime =  DateTime.Parse("2021-09-08")},
+                new Goods {GoodsId = 10001, GoodsName = "Calculator", GoodsNum = 100, GoodsPrice = 19.9, GoodsBuyTime =  DateTime.Parse("2021-09-08")},
+                new Goods {GoodsId = 10001, GoodsName = "Scissors", GoodsNum = 10, GoodsPrice = 4.8, GoodsBuyTime =  DateTime.Parse("2021-09-08")}
             };
             goods.ForEach(s => context.Goods.AddOrUpdate(p => p.GoodsId, s));
             context.SaveChanges();
