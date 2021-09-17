2e6473febe8a1875472c12d69ede1a91b16d74bf
Aiyic <1936014596@qq.com>
2021-09-17 16:04:57 +0800

    Migration-Goods-type

---
+++ Management/Migrations/202109170801202_add_goods_type.cs
@@ -0,0 +1,24 @@
+﻿namespace Management.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class add_goods_type : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.Records");
+            AddColumn("dbo.Goods", "GoodsType", c => c.Int(nullable: false));
+            AlterColumn("dbo.Records", "RecordId", c => c.Int(nullable: false));
+            AddPrimaryKey("dbo.Records", "RecordId");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.Records");
+            AlterColumn("dbo.Records", "RecordId", c => c.Int(nullable: false, identity: true));
+            DropColumn("dbo.Goods", "GoodsType");
+            AddPrimaryKey("dbo.Records", "RecordId");
+        }
+    }
+}
